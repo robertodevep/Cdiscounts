660d2c6af234f4a975c06771d4a4ef9fc948314a
RobertoAfrikpay <kroberto@it.afrikpay.com>
2026-08-11 09:56:07 +0100

    mise a jour

--- app/modal.tsx
+++ app/modal.tsx
@@ -2,7 +2,7 @@ import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { ThemedView } from '@/components/themed-view'; // import the ThemedView component
 
 export default function ModalScreen() {
   return (
