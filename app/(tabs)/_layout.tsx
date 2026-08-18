7f081bf244daae3068c8dc1b902104739846bfb3
RobertoAfrikpay <kroberto@it.afrikpay.com>
2026-08-18 08:26:10 +0100

    generer l'apk

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -18,7 +18,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index" // page accueille
+        name="index" 
         options={{
           title: "Home",
           tabBarIcon: ({ color }) => (
